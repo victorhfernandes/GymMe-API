50fce8c77590eba68b40b96e1f2d62c7e015376a
Victor Hugo Fernandes <victorh.fernandes.o@gmail.com>
2024-09-10 01:31:35 -0300

    organizando select instrutor com especialização

--- src/models/instrutor.model.ts
+++ src/models/instrutor.model.ts
@@ -9,9 +9,58 @@ type PostBody = {
   cref_instrutor: string;
 };
 
+// mover isso para outra pasta
+// pesquisar sobre [Symbol.iterator]() para não utilizar type any
+const organize = (body: any) => {
+  const newJson = [];
+  let idInstrutor = 0;
+  let count = 0;
+
+  for (let item of body) {
+    const objInstrutor = {
+      id_instrutor: item.id_instrutor,
+      nm_instrutor: item.instrutor.nm_instrutor,
+      especializacao: [
+        {
+          id: item.id_especializacao,
+          name: item.especializacao.nm_especializacao,
+        },
+      ],
+    };
+
+    if (item.id_instrutor === idInstrutor) {
+      newJson[count - 1].especializacao.push(objInstrutor.especializacao[0]);
+    } else {
+      newJson.push(objInstrutor);
+      count++;
+    }
+
+    idInstrutor = item.id_instrutor;
+  }
+  return newJson;
+};
+
 export const findInstrutor = async () => {
-  const instrutor = await prisma.instrutor.findMany();
-  return instrutor;
+  const instrutor = await prisma.instrutorEspecializacao.findMany({
+    include: {
+      instrutor: {
+        select: {
+          nm_instrutor: true,
+        },
+      },
+      especializacao: {
+        select: {
+          nm_especializacao: true,
+        },
+      },
+    },
+    orderBy: {
+      instrutor: {
+        nm_instrutor: "asc",
+      },
+    },
+  });
+  return organize(instrutor);
 };
 
 export const findInstrutorById = async (id: number) => {
