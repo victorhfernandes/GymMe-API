856fa86a1f71286ef27f35aa2ecc52116b2ec030
victorhfernandes <victorh.fernandes.o@gmail.com>
2024-09-13 09:17:24 -0300

    add mensagem conexão db

--- src/utils/prisma.util.ts
+++ src/utils/prisma.util.ts
@@ -1,3 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
+
+async function checkDatabaseConnection() {
+  try {
+    await prisma.$connect();
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  } finally {
+    await prisma.$disconnect(); // Ensure disconnection after testing
+  }
+}
+
+checkDatabaseConnection();
